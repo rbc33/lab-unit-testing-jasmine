8ef2adc39c5ed4cd7a3620fd78eb8048c48fb2aa
rbc33 <ricardobenthem@gmail.com>
2025-09-12 14:13:11 +0200

    added test to divide

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -19,36 +19,12 @@ describe('Iteration 2', () => {
 			expect(divide()).toEqual(undefined)
 			expect(divide(undefined, 1)).toEqual(undefined)
 		})
+		it('should return undefined if any of the two arguments is not a number', () => {
+			expect(divide('1', 2)).toEqual(undefined)
+			expect(divide(1, '2')).toEqual(undefined)
+			expect(divide('hello', 'world')).toEqual(undefined)
+			expect(divide(true, 5)).toEqual(undefined)
+			expect(divide(null, 3)).toEqual(undefined)
+		})
 	})
 })
-
-// describe('Iteration 1 | Getting Started', () => {
-// 	describe('Function add', () => {
-// 		it('should be defined', () => {
-// 			expect(divide).toBeDefined()
-// 		})
-
-// 		it('should take two arguments', () => {
-// 			expect(divide.length).toBe(2)
-// 		})
-
-// 		it('should return the sum of the two numbers', () => {
-// 			expect(divide(1, 2)).toEqual(3)
-// 			expect(divide(3, 4)).toEqual(7)
-// 			expect(divide(100, 47)).toEqual(147)
-// 		})
-
-// 		it('should return undefined if any of the arguments is not provided', () => {
-// 			expect(divide(1)).toEqual(undefined)
-// 			expect(divide()).toEqual(undefined)
-// 			expect(divide(undefined, 1)).toEqual(undefined)
-// 		})
-// 		it('should return undefined if any of the two arguments is not a number', () => {
-// 			expect(divide('1', 2)).toEqual(undefined)
-// 			expect(divide(1, '2')).toEqual(undefined)
-// 			expect(divide('hello', 'world')).toEqual(undefined)
-// 			expect(divide(true, 5)).toEqual(undefined)
-// 			expect(divide(null, 3)).toEqual(undefined)
-// 		})
-// 	})
-// })
